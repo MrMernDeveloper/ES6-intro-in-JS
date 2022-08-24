e1f5a80d33291693d5570178fd2864ad355e1b21
MrMernDeveloper <rakibkst16@gmail.com>
2022-08-25 01:17:51 +0600

    added some text

--- js/let-cons.js
+++ js/let-cons.js
@@ -10,13 +10,50 @@ number = 900;
 console.log(number);
 */
 
-const numbers = [11, 12, 13, 13, 14];
+// const numbers = [11, 12, 13, 13, 14];
 
-// myArry.push(112);
-// console.log(myArry);
+// // myArry.push(112);
+// // console.log(myArry);
 
-for (let i = 0; i < numbers.length; i++){
-    let element = numbers[i];
+// for (let i = 0; i < numbers.length; i++){
+//     let element = numbers[i];
     
+// }
+// console.log(element);
+
+
+
+// evaluation of ES-6 
+function add(number1, number2) {
+    result = number1 + number2;
+    return result;
 }
-console.log(element);
+
+const add6 = (number1, number2) => number1 + number2;
+
+const result = add6(20, 30);
+console.log(result);
+
+// arrow function 
+const mutiply = (number1, number2) => number1 * number2;
+const yourResult = mutiply(10, 10);
+console.log(yourResult);
+
+const getPie = () => 3.1416;
+
+console.log(getPie())
+
+const fiveTimes = number => number * 5;
+console.log(fiveTimes(5));
+
+// multiline code
+
+const doMath = (x, y, z) => {
+    
+    const number1 = x + y;
+    const number2 = y + z;
+    const numberMultiply = number1 * number2;
+    const result = numberMultiply / 2;
+    return result;
+}
+console.log(doMath(1, 2, 3));
---
+++ spread.js
@@ -0,0 +1,90 @@
+/*
+const max = Math.max(1, 2, 5, 7, 9);
+// console.log(max);
+
+// spread operator  ...
+
+const myArry = [10, 10, 20, 40, 50];
+
+const height = Math.max(...myArry);
+// console.log(...myArry);
+// console.log(height);
+
+// another example of spread operator
+
+const firstArry = [1, 2, 34, 5, 45, 65];
+const secondAryy = [...firstArry];
+const thirdArry = [ 22,23, ...firstArry, 88]
+firstArry.push(11);
+console.log(firstArry)
+console.log(secondAryy);
+console.log(thirdArry);
+
+const fish = {
+    name: 'king Hilsha',
+    address: 'chandpu',
+    color: 'silver',
+    phone: '01767968526',
+    price: 400,
+}
+
+const phone = fish.phone;
+
+console.log(phone);
+
+const { address } = fish;
+console.log(address);
+
+const { age, name } = { name: 'almas', age: '30', phone: '01767968526' };
+
+console.log(age, name);
+
+
+// arry discructing
+
+const newArry = [10, 20, 30, 50];
+const [first, second, third] = newArry;
+console.log(first);
+
+const actors = ['bappraj', 'alim', 'sabana'];
+
+const [firstActors, secondActors] = actors;
+console.log(firstActors);
+
+//
+
+function getNames() {
+    return ['halim', 'alim']
+}
+
+const [baba, chacha] = getNames();
+console.log(chacha, baba);
+//
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon", "Pineapple");
+console.log(fruits);
+
+
+function getSum(a, b = 9) { return a + b; } console.log(getSum(2, 7));
+
+const y = x => x * x; const z = y(5); 
+console.log(z);
+*/
+
+
+
+// const name = 'Hero';
+// const greetings = 'welcome home!' + ' ' + ${ name };
+// console.log(greetings);
+
+// problem -1
+// const myFunction = (number1, number2, number3) => number1 * number2 * number3;
+// console.log(myFunction(2, 3, 5));
+
+// problem -2
+
+
+
+
